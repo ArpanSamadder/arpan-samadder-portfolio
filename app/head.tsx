8d94be71b243f54eac4e55a89d4e4d185bbd5b1c
Arpan Samadder <20223056307arpan@juniv.edu>
2026-06-13 04:46:53 +0600

    Add heading override in app head

--- app/head.tsx
+++ app/head.tsx
@@ -1,3 +1,12 @@
 export default function Head() {
-  return null;
+  return (
+    <style>{`
+      #\\70\\72\\6f\\66\\69\\6c\\65-title:before {
+        font-size: 37px !important;
+      }
+      #\\70\\72\\6f\\66\\69\\6c\\65-title:after {
+        font-size: 46px !important;
+      }
+    `}</style>
+  );
 }
