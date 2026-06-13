c33aec5d5934d6048c53184d00c17869e20d535e
Arpan Samadder <20223056307arpan@juniv.edu>
2026-06-13 06:00:01 +0600

    Import ReactNode in Hire Me layout

--- app/hire-me/layout.tsx
+++ app/hire-me/layout.tsx
@@ -1,5 +1,6 @@
+import type { ReactNode } from "react";
 import "./hire-fixes.css";
 
-export default function Layout(props: { children: React.ReactNode }) {
-  return props.children;
+export default function Layout({ children }: { children: ReactNode }) {
+  return children;
 }
